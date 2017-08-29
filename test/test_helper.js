922000804ebb5aeb213458d24f13f3830d929413
Jade Bautista <edaj.bautista@gmail.com>
2017-08-29 12:46:28 +0800

    Update jsdom syntax for helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,16 +2,17 @@ import _$ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
-import jsdom from 'jsdom';
+import { JSDOM } from 'jsdom';
 import chai, { expect } from 'chai';
 import chaiJquery from 'chai-jquery';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+global.window = jsdom.window;
+global.document = window.document;
+global.navigator = window.navigator;
 const $ = _$(window);
 
 chaiJquery(chai, chai.util, $);
