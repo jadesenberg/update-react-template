158862d50e722c767515130130623855240fdb59
Jade Bautista <edaj.bautista@gmail.com>
2017-09-19 23:46:25 +0800

    change template content

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>Test template</div>
+      <div>Updated Templat for react-redux</div>
     );
   }
 }
